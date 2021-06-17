bb1de7b22713e3168c6f15679daeac1f5da18343
Hiroki Mori <xxx.bvb.taylor@gmail.com>
2021-06-18 07:43:43 +0900

    Lintエラーの修正

--- web/src/test/components/molecules/SourceInList.test.tsx
+++ web/src/test/components/molecules/SourceInList.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
 import SourceInList from 'src/components/molecules/SourceInList'
 import { ISource } from 'src/type'
