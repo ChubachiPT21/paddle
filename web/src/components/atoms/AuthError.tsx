71ec2dfadba7397d3349ff414eb35c93dfdf8ca5
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2021-06-17 23:10:43 +0900

    [ch715]

--- web/src/components/atoms/AuthError.tsx
+++ web/src/components/atoms/AuthError.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { IUserState } from 'src/reducers/userReducers'
 
-const AuthError: FC = () => (
-  <div className="auth-error">Email or password is invalid</div>
-)
+const AuthError: FC = () => {
+  const authenticationError = useSelector(
+    (state: { user: IUserState }) => state.user.error
+  )
+
+  if (authenticationError) {
+    return (
+      <div className="auth-error">Email or password is invalid</div>
+    )
+  }
+  return <div />
+}
 
 export default AuthError
