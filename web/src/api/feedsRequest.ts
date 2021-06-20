dcb7b4cc061d33b4594d76ddb3fad621c1280a3d
mr-myself <keisuke@mr-myself.com>
2021-06-20 10:24:06 +0900

    post request when clicking home icon

--- web/src/api/feedsRequest.ts
+++ web/src/api/feedsRequest.ts
@@ -5,6 +5,9 @@ const feedsRequest = {
   fetchFeeds: (sourceId: number) => {
     return axios.get<IFeed[]>(`/v1/sources/${sourceId}/feeds`)
   },
+  createFeeds: () => {
+    return axios.post<any>(`/v1/feeds`)
+  },
 }
 
 export default feedsRequest
