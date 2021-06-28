4ae35ac2fd862489a8a66d708c40100d6f4173c4
chubachi+pt+circleci <chubachi+pt+circleci@users.noreply.github.com>
2021-06-28 07:38:39 +0000

    [skip ci] lintfix

--- web/src/components/molecules/HeaderRightMenu.tsx
+++ web/src/components/molecules/HeaderRightMenu.tsx
@@ -1,18 +1,15 @@
 import React, { FC } from 'react'
 import { signOut } from 'src/actions/authenticationActions'
 import { IUser } from 'src/type'
-import { useDispatch } from 'react-redux'
-import { Provider } from 'react-redux'
+import { useDispatch, Provider } from 'react-redux'
+
 import store from '../../store'
-
-
 
 type Props = {
   user: IUser
 }
 
 const HeaderRightMenu: FC<Props> = ({ user }) => {
-
   const onClick = async () => {
     const dispatch = useDispatch()
     await dispatch(signOut())
@@ -21,40 +18,43 @@ const HeaderRightMenu: FC<Props> = ({ user }) => {
 
   const configuredStore = store
 
-
   return (
     <Provider store={configuredStore}>
-    <div className="rightMenu">
-      {user.token && (
-        <span>
-          Welcome! {user.email}
-          <span
-            id="right-menu-sign-out"
-            className="rightMenu__item rightMenu__item--border pointer"
-            onKeyDown={onClick}
-            onClick={onClick}
-            role="link"
-            tabIndex={0}
-          >
-            Sign Out
+      <div className="rightMenu">
+        {user.token && (
+          <span>
+            Welcome! {user.email}
+            <span
+              id="right-menu-sign-out"
+              className="rightMenu__item rightMenu__item--border pointer"
+              onKeyDown={onClick}
+              onClick={onClick}
+              role="link"
+              tabIndex={0}
+            >
+              Sign Out
+            </span>
           </span>
-        </span>
-      )}
-      {!user.token && (
-        <>
-          <a id="right-menu-sign-in" href="/signin" className="rightMenu__item">
-            Sign In
-          </a>
-          <a
-            id="right-menu-sign-up"
-            href="/signup"
-            className="rightMenu__item rightMenu__item--border"
-          >
-            Sign Up
-          </a>
-        </>
-      )}
-    </div>
+        )}
+        {!user.token && (
+          <>
+            <a
+              id="right-menu-sign-in"
+              href="/signin"
+              className="rightMenu__item"
+            >
+              Sign In
+            </a>
+            <a
+              id="right-menu-sign-up"
+              href="/signup"
+              className="rightMenu__item rightMenu__item--border"
+            >
+              Sign Up
+            </a>
+          </>
+        )}
+      </div>
     </Provider>
   )
 }
